64c8a698a2d9c697c94d8dc41176257412bbcc98
Sylvester <36532468+SylvesterWilmott@users.noreply.github.com>
2023-02-17 13:24:58 +0000

    Improve the way storage changes are handled (#1)
    * Improve the way storage changes are handled
    
    * Run standardjs

--- src/strings.js
+++ src/strings.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict'
 
 const uiStrings = {
-  ADD: "Add",
-  CLEAR_ALL: "Clear All",
-  PREFERENCES: "Preferences",
-  OPEN_AT_LOGIN: "Open at Login",
-  QUIT: "Quit",
-  CHOOSE_PROMPT: "Choose a file or folder",
-  CHOOSE: "Choose",
-  FIND_FILE: "Find File/Folder...",
-  REMOVE: "Remove",
-  OPEN: "Open",
-  OPEN_IN_FOLDER: "Reveal in Finder",
-  ALREADY_EXISTS: "File/Folder Already Added",
-  FILE_NOT_FOUND: "File Not Found",
-  FILE_NOT_FOUND_DETAIL: "Find the location of your file or remove it"
-};
+  ADD: 'Add',
+  CLEAR_ALL: 'Clear All',
+  PREFERENCES: 'Preferences',
+  OPEN_AT_LOGIN: 'Open at Login',
+  QUIT: 'Quit',
+  CHOOSE_PROMPT: 'Choose a file or folder',
+  CHOOSE: 'Choose',
+  FIND_FILE: 'Find File/Folder...',
+  REMOVE: 'Remove',
+  OPEN: 'Open',
+  OPEN_IN_FOLDER: 'Reveal in Finder',
+  ALREADY_EXISTS: 'File/Folder Already Added',
+  FILE_NOT_FOUND: 'File Not Found',
+  FILE_NOT_FOUND_DETAIL: 'Find the location of your file or remove it'
+}
 
-module.exports = uiStrings;
+module.exports = uiStrings
